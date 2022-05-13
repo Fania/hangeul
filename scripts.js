5c19b3796cd67439c618f8706679f358596d8200
Fania Everitt <me@fania.eu>
2022-05-13 18:26:20 +0100

    add syllables table

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 console.log('hello world');
 
 
-let vowels = document.querySelectorAll(".vowels div");
+// let vowels = document.querySelectorAll(".vowels div");
 
-vowels.forEach( vs => {
-    console.log(vs,'test');
-    // vs.classList.add("tmp");
-});
+// vowels.forEach( vs => {
+//     console.log(vs,'test');
+//     // vs.classList.add("tmp");
+// });
