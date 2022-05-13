8ac27946ec2b924126c9367dec6f4bb074a0a064
Fania Everitt <me@fania.eu>
2022-05-13 20:33:38 +0100

    add hammer and prototype en/ko swiping

--- scripts.js
+++ scripts.js
@@ -7,3 +7,46 @@ console.log('hello world');
 //     console.log(vs,'test');
 //     // vs.classList.add("tmp");
 // });
+
+
+// vowels.forEach( vs => {
+//     // console.dir(vs);
+//     const alt = vs.dataset.alt;
+//     console.log(alt);
+// });
+
+
+
+
+
+
+
+const vowels = document.querySelectorAll(".vowels div");
+const vowelSection = document.querySelector(".vowels");
+function changeVowels(ev,lang) {
+  // console.dir(vs);
+  if(lang=='en') {
+    console.log(ev.type,'english');
+    vowels.forEach( vs => {
+      vs.innerHTML = vs.dataset.en;
+    });
+    vowelSection.classList = 'english vowels';
+  } else {
+    console.log(ev.type,'korean');
+    vowels.forEach( vs => {
+      vs.innerHTML = vs.dataset.ko;
+    });
+    vowelSection.classList = 'korean vowels';
+  }
+}
+// changeVowels('ko');
+// changeVowels('en');
+
+
+const mc = new Hammer(vowelSection);
+mc.get('swipe').set({ enable: true, direction: Hammer.DIRECTION_HORIZONTAL });
+mc.get('pan').set({ enable: true, direction: Hammer.DIRECTION_HORIZONTAL });
+mc.on("swipeleft",  (ev) => { changeVowels(ev,"ko"); });
+mc.on("panleft",  (ev) => { changeVowels(ev,"ko"); });
+mc.on("swiperight", (ev) => { changeVowels(ev,"en"); });
+mc.on("panright", (ev) => { changeVowels(ev,"en"); });
