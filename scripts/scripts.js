c9c703d44ca091f24220ae7e45be29cfb44209ba
Fania Everitt <me@fania.eu>
2022-06-17 15:33:15 +0100

    add all english translations

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -39,14 +39,58 @@ function changeVowels(ev,lang) {
     vowelSection.classList = 'korean vowels';
   }
 }
-// changeVowels('ko');
-// changeVowels('en');
+
+const consonants = document.querySelectorAll(".consonants div");
+const consonantSection = document.querySelector(".consonants");
+function changeConsonants(ev,lang) {
+  // console.dir(vs);
+  if(lang=='en') {
+    console.log(ev.type,'english');
+    consonants.forEach( vs => {
+      vs.innerHTML = vs.dataset.en;
+    });
+    consonantSection.classList = 'english consonants';
+  } else {
+    console.log(ev.type,'korean');
+    consonants.forEach( vs => {
+      vs.innerHTML = vs.dataset.ko;
+    });
+    consonantSection.classList = 'korean consonants';
+  }
+}
+
+const syllables = document.querySelectorAll(".syllables div");
+const syllableSection = document.querySelector(".syllables");
+function changeSyllables(ev,lang) {
+  // console.dir(vs);
+  if(lang=='en') {
+    console.log(ev.type,'english');
+    syllables.forEach( vs => {
+      vs.innerHTML = vs.dataset.en;
+    });
+    syllableSection.classList = 'english syllables';
+  } else {
+    console.log(ev.type,'korean');
+    syllables.forEach( vs => {
+      vs.innerHTML = vs.dataset.ko;
+    });
+    syllableSection.classList = 'korean syllables';
+  }
+}
 
 
-const mc = new Hammer(vowelSection);
-mc.get('swipe').set({ enable: true, direction: Hammer.DIRECTION_HORIZONTAL });
+const mcv = new Hammer(vowelSection);
+const mcc = new Hammer(consonantSection);
+const mcs = new Hammer(syllableSection);
+mcv.get('swipe').set({ enable: true, direction: Hammer.DIRECTION_HORIZONTAL });
+mcc.get('swipe').set({ enable: true, direction: Hammer.DIRECTION_HORIZONTAL });
+mcs.get('swipe').set({ enable: true, direction: Hammer.DIRECTION_HORIZONTAL });
 // mc.get('pan').set({ enable: true, direction: Hammer.DIRECTION_HORIZONTAL });
-mc.on("swipeleft",  (ev) => { changeVowels(ev,"ko"); });
+mcv.on("swipeleft",  (ev) => { changeVowels(ev,"ko"); });
+mcc.on("swipeleft",  (ev) => { changeConsonants(ev,"ko"); });
+mcs.on("swipeleft",  (ev) => { changeSyllables(ev,"ko"); });
 // mc.on("panleft",  (ev) => { changeVowels(ev,"ko"); });
-mc.on("swiperight", (ev) => { changeVowels(ev,"en"); });
+mcv.on("swiperight", (ev) => { changeVowels(ev,"en"); });
+mcc.on("swiperight", (ev) => { changeConsonants(ev,"en"); });
+mcs.on("swiperight", (ev) => { changeSyllables(ev,"en"); });
 // mc.on("panright", (ev) => { changeVowels(ev,"en"); });
